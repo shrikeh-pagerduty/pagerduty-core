962f6587a19f8f4cd184e95494427c7ab600add3
Barney Hanlon <shrikeh@gmail.com>
2016-10-13 17:07:20 +0100

    Tweak

--- src/Collection/Traits/ThrowImmutable.php
+++ src/Collection/Traits/ThrowImmutable.php
@@ -6,6 +6,10 @@ use Shrikeh\PagerDuty\Collection\Exception\ImmutableCollectionException;
 
 trait ThrowImmutable
 {
+    /**
+     * Overrides the default DomainException thrown for immutable collections.
+     * @throws Shrikeh\PagerDuty\Collection\Exception\ImmutableCollectionException
+     */
     protected function throwImmutable($msg, $errorCode = null)
     {
         throw new ImmutableCollectionException($msg, $errorCode);
